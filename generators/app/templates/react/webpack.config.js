2172ff764357964bccc66f2634c2fe56c5eed6e6
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-08 05:39:09 -0500

    updated structure

--- generators/app/templates/react/webpack.config.js
+++ generators/app/templates/react/webpack.config.js
@@ -11,11 +11,11 @@ const {
 } = require('@webpack-blocks/webpack2')
 
 const host = process.env.HOST || 'localhost'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || <%= clientPortDev %>
 const sourceDir = process.env.SOURCE || 'src'
 const publicPath = `/${process.env.PUBLIC_PATH || ''}/`.replace('//', '/')
 const sourcePath = path.join(process.cwd(), sourceDir)
-const outputPath = path.join(process.cwd(), 'dist')
+const outputPath = path.join(process.cwd(), '<%= pathDistClient %>')
 
 const babel = () => () => ({
   module: {
