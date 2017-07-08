53aba1de56427b9c49f6fbc8ab90893d1a2d7e36
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-08 05:36:19 -0500

    updated compiler

--- generators/app/index.js
+++ generators/app/index.js
@@ -26,7 +26,9 @@ module.exports = class extends Generator {
         type: "list",
         name: "stack",
         message: "Which stack do you want?",
-        choices: ["fullstack", "client", "server"],
+        choices: [
+          "fullstack", "client", "server"
+        ],
         default: 0
       }, {
         type: "list",
@@ -36,12 +38,13 @@ module.exports = class extends Generator {
           name: 'ReactJS (ARC)',
           value: 'react'
         }, {
-          name: 'VueJS',
-          value: 'vuejs'
+          name: 'VueJS (Soon)',
+          value: 'vue'
         }, {
-          name: 'Angular (CLI',
+          name: 'Angular (Soon)',
           value: 'angular'
         }],
+        store: true,
         default: 0,
         when: (answersHash) => {
           return answersHash.stack !== "server";
@@ -77,7 +80,7 @@ module.exports = class extends Generator {
         message: 'Want multiple login? (If you only want one device at the time select N)',
         default: 'Y',
         when: (answersHash) => {
-          return answersHash.stack !== "auth";
+          return answersHash.auth;
         }
       }, {
         type: "checkbox",
@@ -149,70 +152,154 @@ module.exports = class extends Generator {
   }
 
   configuring() {
-    //Readme
-    // this.fs.copy(this.templatePath('server/README.md'), this.destinationPath(
-    //   'README.md'));
-    //package.json
 
-    // this.fs.copyTpl(this.templatePath('server/package.json'), this.destinationPath(
-    //   'server/package.json'), {
-    //   name: this.props.name,
-    //   description: this.props.description,
-    // });
+    let npm = {
+      start: '',
+      install: '',
+      build: '',
+      test: ''
+    }
+
+    //package.json
+    if (this.props.stack != 'client') {
+      npm.start += " 'cd server && npm start' ";
+      npm.install += " 'cd server && npm i' ";
+      npm.build += " 'cd server && npm run build' ";
+      npm.test += " 'cd server && npm test' ";
+    }
+
+    if (this.props.stack != 'server') {
+      switch (this.props.client) {
+        case 'react':
+          npm.start += " 'cd react && npm start' ";
+          npm.install += " 'cd react && npm i' ";
+          npm.build += " 'cd react && npm run build' ";
+          break;
+        case 'vue':
+          npm.start += " 'cd vue && npm start' ";
+          npm.install += " 'cd vue && npm i' ";
+          npm.build += " 'cd vue && npm run build' ";
+          break;
+        case 'angular':
+          npm.start += " 'cd angular && npm start' ";
+          npm.install += " 'cd angular && npm i' ";
+          npm.build += " 'cd angular && npm run build' ";
+          break;
+        default:
+      }
+    }
+
+    this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath(
+      'package.json'), {
+      name: this.props.name,
+      npm: npm
+    });
 
   }
 
   writing() {
 
-    console.log('->', this.props);
+    // console.log('->', this.props);
 
-    return;
     //<----------Server---------->
 
-    this.fs.copy(
-      this.templatePath('server/**/*'),
-      this.destinationPath('server')
-    );
+    if (this.props.stack != 'client') {
 
-    this.fs.copyTpl(this.templatePath('server/package.json'), this.destinationPath(
-      'server/package.json'), {
-      name: this.props.name,
-      description: this.props.description,
-    });
+      this.fs.copy(
+        this.templatePath('server/**/*'),
+        this.destinationPath('server')
+      );
 
-    this.fs.copyTpl(this.templatePath('server/src/config'), this.destinationPath(
-      'server/src/config'), {
-      name: this.props.name
-    });
+      let optionsServer = {
+        name: this.props.name,
+        multipleDevices: this.props.multipleDevices || false,
+        auth: {
+          enabled: this.props.auth,
+          local: this.props.auth && (this.props.authList.indexOf('local') > -1),
+          google: this.props.auth && (this.props.authList.indexOf('google') > -1),
+          facebook: this.props.auth && (this.props.authList.indexOf('facebook') > -1),
+          twitter: this.props.auth && (this.props.authList.indexOf('twitter') > -1),
+          github: this.props.auth && (this.props.authList.indexOf('github') > -1),
+          bitbucket: this.props.auth && (this.props.authList.indexOf('bitbucket') > -1)
+        },
+        serverPortDev: this.props.serverPortDev,
+        serverPortPro: this.props.serverPortPro,
+        pathDistServerPro: `${this.props.client}`
+      }
 
-    this.fs.copy(
-      this.templatePath('server/.*'),
-      this.destinationPath('server')
-    );
+      this.fs.copyTpl(this.templatePath('server/**/*.js'), this.destinationPath(
+        'server'), optionsServer);
 
-    //<----------React---------->
+      this.fs.copyTpl(this.templatePath('server/*'), this.destinationPath(
+        'server'), optionsServer);
 
-    this.fs.copy(
-      this.templatePath('react/**/*'),
-      this.destinationPath('react')
-    );
+      this.fs.copy(this.templatePath('server/.*'), this.destinationPath(
+        'server'));
 
-    // React Copy all dotfiles
-    this.fs.copy(
-      this.templatePath('react/.*'),
-      this.destinationPath('react')
-    );
+    }
+
+
+    //<----------Client---------->
+
+    if (this.props.stack != 'server') {
+
+      switch (this.props.client) {
+        //<----------React---------->
+        case 'react':
+
+          this.fs.copy(this.templatePath('react/**/*'), this.destinationPath(
+            'react'));
+
+          let optionsReact = {
+            name: this.props.name,
+            client: `${this.props.client}`,
+            clientPortDev: this.props.clientPortDev,
+            pathDistClient: `../server/dist/${this.props.client}`
+              // pathDistClient: `dist`
+          }
+
+          this.fs.copyTpl(this.templatePath('react/webpack.config.js'),
+            this.destinationPath(
+              'react/webpack.config.js'), optionsReact);
+
+          this.fs.copyTpl(this.templatePath('react/package.json'), this.destinationPath(
+            'react/package.json'), optionsReact);
+
+          // React Copy all dotfiles
+          this.fs.copy(this.templatePath('react/.*'), this.destinationPath(
+            'react'));
+
+          break;
+
+          //<----------React---------->
+        case 'vue':
+
+          this.fs.copy(this.templatePath('vue/**/*'), this.destinationPath(
+            'vue'));
+
+          break;
+          //<----------Angular---------->
+        case 'angular':
+
+          this.fs.copy(this.templatePath('angular/**/*'), this.destinationPath(
+            'angular'));
+
+          break;
+        default:
+      }
+
+    }
 
   }
 
   initializing() {
 
     let options = {
-      arguments: this.arguments
+      arguments: this.arguments,
+      props: this.props,
     };
 
     //<----------Server---------->
-
     this.composeWith('scaling-fullstack:server', {
       options: options
     }, {
@@ -220,25 +307,33 @@ module.exports = class extends Generator {
     });
 
     //<----------React---------->
-
     this.composeWith('scaling-fullstack:react', {
       options: options
     }, {
       local: require.resolve('../react')
     });
 
+    //<----------Vue---------->
+    this.composeWith('scaling-fullstack:vue', {
+      options: options
+    }, {
+      local: require.resolve('../vue')
+    });
+
+    //<----------Angular---------->
+    this.composeWith('scaling-fullstack:angular', {
+      options: options
+    }, {
+      local: require.resolve('../angular')
+    });
 
   }
 
-  _private_method(param) {
+  // Private methods
 
+  _private_server() {
 
-  }
-
-  install() {
-    // let npmdir = process.cwd() + '/server';
-    if (!this.options['skip-install']) {
-      //<----------Server---------->
+    if (this.props.stack != 'client') {
       process.chdir(`${this.destinationRoot()}/server`);
       this.installDependencies({
         npm: true,
@@ -246,26 +341,50 @@ module.exports = class extends Generator {
         yarn: false,
         callback: () => {
           console.log(`npm Server is installed!`);
-          //<----------React---------->
-          process.chdir(`${this.destinationRoot()}/react`);
-          this.installDependencies({
-            npm: true,
-            bower: false,
-            yarn: false,
-            callback: () => {
-              console.log(`npm React is installed!`);
-            }
-          });
-          //<----------Vue---------->
-          //<----------Angular---------->
         }
       });
     }
+
   }
 
+  _private_client() {
+
+    process.chdir(`${this.destinationRoot()}/${this.props.client}`);
+    this.installDependencies({
+      npm: true,
+      bower: false,
+      yarn: false,
+      callback: () => {
+        console.log(`npm Client is installed!`);
+        this._private_server();
+      }
+    });
+
+  }
+
+  install() {
+
+    this.installDependencies({
+      npm: true,
+      bower: false,
+      yarn: false,
+      callback: () => {
+        // if (this.props.stack === 'server') {
+        //this._private_server();
+        // } else {
+        //this._private_client();
+        // }
+      }
+    });
+
+  }
 
   end() {
-    console.log('END');
+
+    console.log(chalk.greenBright(
+      '\n-----------\nSCALING-FULLSTACK SUCCESS!\n-----------\n'));
   }
+
+
 
 };
