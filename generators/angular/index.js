9b2d98864380c6db81c0b50de730c2cef801fe67
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-08 05:36:54 -0500

    yo angular

---
+++ generators/angular/index.js
@@ -0,0 +1,15 @@
+const Generator = require('yeoman-generator');
+module.exports = class extends Generator {
+
+  initializing() {
+    // console.log('angular 1');
+  }
+
+  prompting() {
+    // console.log(this.args);
+    // console.log('angular - prompting');
+  }
+  writing() {
+    // console.log('angular - writing');
+  }
+};
