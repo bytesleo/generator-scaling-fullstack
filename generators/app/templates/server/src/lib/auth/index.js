0391b45c4e90aa98d0f0e3f23b3216704a9e2830
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-08 05:42:30 -0500

    yeoman validation

--- generators/app/templates/server/src/lib/auth/index.js
+++ generators/app/templates/server/src/lib/auth/index.js
@@ -13,22 +13,43 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-//initialize passport
-require('./local/local.passport').setup(User, config);
-require('./github/github.passport').setup(User, config);
-// require('./twitter/twitter.passport').setup(User, config);
-// require('./facebook/facebook.passport').setup(User, config);
-// require('./google/google.passport').setup(User, config);
-// require('./bitbucket/bitbucket.passport').setup(User, config);
-
-//Routers
 const router = express.Router();
 
-router.use('/local', require('./local').default);
-router.use('/github', require('./github').default);
-// router.use('/twitter', require('./twitter').default);
-// router.use('/facebook', require('./facebook').default);
-// router.use('/google', require('./google').default);
-// router.use('/bitbucket', require('./bitbucket').default);
-
+//Passport
+<% if(auth.local){ %>
+if (config.oAuth.local.enabled) {
+  require('./local/local.passport').setup(User, config);
+  router.use('/local', require('./local').default);
+}
+<% } %>
+<% if(auth.github){ %>
+if (config.oAuth.github.enabled) {
+  require('./github/github.passport').setup(User, config);
+  router.use('/github', require('./github').default);
+}
+<% } %>
+<% if(auth.twitter){ %>
+if (config.oAuth.twitter.enabled) {
+  require('./twitter/twitter.passport').setup(User, config);
+  router.use('/twitter', require('./twitter').default);
+}
+<% } %>
+<% if(auth.facebook){ %>
+if (config.oAuth.facebook.enabled) {
+  require('./facebook/facebook.passport').setup(User, config);
+  router.use('/facebook', require('./facebook').default);
+}
+<% } %>
+<% if(auth.google){ %>
+if (config.oAuth.google.enabled) {
+  require('./google/google.passport').setup(User, config);
+  router.use('/google', require('./google').default);
+}
+<% } %>
+<% if(auth.bitbucket){ %>
+if (config.oAuth.bitbucket.enabled) {
+  require('./bitbucket/bitbucket.passport').setup(User, config);
+  router.use('/bitbucket', require('./bitbucket').default);
+}
+<% } %>
 export default router;
